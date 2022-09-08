fbcfd561f908db10ed6d18f19029a72ea536afb8
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-09-08 17:18:34 +0530

    Division1.kt source code file added

---
+++ Basic Programs/Division1.kt
@@ -0,0 +1,31 @@
+// Division1.kt
+
+/**
+ * Wap to calculate and display the division of two numbers on the console screen.
+ * Where, num1 = 225 and num2 = 15
+ * Date: Thu, 08 Sep, 2022
+ * @author: ANKUR SAXENA
+ * Platform: Linux Ubuntu 22.04/Lts/x64/Kotlin 1.7.10/VS Code
+ */
+
+fun divOfTwoNo1(){
+
+    var num1 = 225
+    var num2 = 17
+
+    // printing values
+    println ("First number is = ${num1}")
+    println ("Second number is = ${num2}")
+
+    // calculating division
+    var divisionResult : Double = num1.toDouble() / num2.toDouble()
+
+    // printing result
+    println ("The division of ${num1} and ${num2} is = ${divisionResult}")
+}
+
+fun main (){
+
+    // calling function
+    println ("${divOfTwoNo1()}")
+}
