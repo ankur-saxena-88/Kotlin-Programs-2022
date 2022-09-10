669836fad6cc12686979430afae933eccd432f13
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-09-10 11:14:35 +0530

    MultiplicationTable2.kt source code file added

---
+++ Basic Programs/MultiplicationTable2.kt
@@ -0,0 +1,35 @@
+// MultiplicationTable2.kt
+
+/**
+ * Wap to display the multiplication table of 25 on the console screen.
+ * Date: Sat, 10 Sep, 2022
+ * @author: ANKUR SAXENA
+ * Platform: Linux Ubuntu 22.04/Lts/x64/Kotlin 1.7.10/VS Code
+ */
+
+fun main(args : Array <String>){
+
+    // calling function
+    multiTable2()
+}
+
+fun multiTable2(){
+
+    var num : Int = 25
+
+    println ("The multiplication table of ${num} is given below...\n")
+
+    // printing table
+    println ("${num} x 1 = ${num * 1}")
+    println ("${num} x 2 = ${num * 2}")
+    println ("${num} x 3 = ${num * 3}")
+    println ("${num} x 4 = ${num * 4}")
+    println ("${num} x 5 = ${num * 5}")
+    println ("${num} x 6 = ${num * 6}")
+    println ("${num} x 7 = ${num * 7}")
+    println ("${num} x 8 = ${num * 8}")
+    println ("${num} x 9 = ${num * 9}")
+    println ("${num} x 10 = ${num * 10}")
+    println ("${num} x 11 = ${num * 11}")
+    println ("${num} x 12 = ${num * 12}")
+}
