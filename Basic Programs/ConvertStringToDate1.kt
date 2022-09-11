b39e211b50e661c283bd17daacbf905dd5ad8375
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-09-11 12:57:07 +0530

    ConvertStringToDate1.kt source code file added

---
+++ Basic Programs/ConvertStringToDate1.kt
@@ -0,0 +1,33 @@
+// ConvertStringToDate.kt
+
+/**
+ * Wap to convert String to Date using pattern formatters.
+ * Date: Sun, 11 Sep, 2022
+ * @author: ANKUR SAXENA
+ * Platform: Linux Ubuntu 22.04/Lts/x64/Kotlin 1.7.0/VS Code
+ */
+
+// importing java packages
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+// creating function
+fun convertStringDate1(){
+
+    // format DD Month, YYYY
+    val string = "11 September, 2022"
+    val formatter = DateTimeFormatter.ofPattern ("d MMMM, yyyy", Locale.ENGLISH)
+
+    val date = LocalDate.parse (string, formatter)
+
+    // printing date
+    println ("${date}")
+}
+
+// creating main function
+fun main(){
+
+    // calling function
+    println ("${convertStringDate1()}")
+}
