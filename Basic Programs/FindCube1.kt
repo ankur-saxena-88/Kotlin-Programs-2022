3bfb66589e7303339ca8baa0aeda614fc4a56519
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-09-17 13:59:47 +0530

    Kotlin program to find the cube of 6 source file added

---
+++ Basic Programs/FindCube1.kt
@@ -0,0 +1,15 @@
+// FindCube1.kt
+
+// Write a Kotlin program to find the cube od 6.
+// Date: Sat, 17 Sep, 2022
+
+fun main(){
+
+    var num = 6
+
+    // calculate cube
+    var result = (num * num) * num
+
+    // printing result
+    println ("The cube of ${num} is : ${result}")
+}
