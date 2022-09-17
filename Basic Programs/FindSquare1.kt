d433a0782d67f6c54b2814f6ec20d66d0e51f20a
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-09-17 13:58:33 +0530

    Kotlin program to find square of 12 added

---
+++ Basic Programs/FindSquare1.kt
@@ -0,0 +1,15 @@
+// FindSquare1.kt
+
+// Write a Kotlin program to find the square od 12.
+// Date: Sat, 17 Sep, 2022
+
+fun main(){
+    
+    var num = 12
+
+    // calculate square
+    var result = num * num
+
+    // printing result
+    println ("The square of ${num} is : ${result}")
+}
