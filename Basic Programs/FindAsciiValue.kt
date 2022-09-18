788b752b686a0f4c72408c82b4794dd2b6269042
Ankur Saxena <46398844+ankur-saxena-88@users.noreply.github.com>
2022-09-18 17:09:58 +0530

    Find ASCII value of a character kotlin source code file added

---
+++ Basic Programs/FindAsciiValue.kt
@@ -0,0 +1,26 @@
+
+
+import kotlin.reflect.typeOf// FindAsciiValue.kt
+
+/**
+ * Write a kotlin program to find the ASCII value of a character.
+ * Where, character = 'c'
+ * Date: Sun, 18 Sep, 2022
+ */
+
+// creating function
+fun findAsciiValue(){
+
+    val character1 = 'c'
+    val asciiValue = character1.code
+
+    // printing ascii value
+    println ("The ASCII value of ${character1} is : ${asciiValue}")
+}
+
+// creating main function
+fun main(){
+
+    // calling function
+    println ("${findAsciiValue()}")
+}
